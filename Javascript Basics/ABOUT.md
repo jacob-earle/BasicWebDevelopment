e960912c6d3883de2ca76e8e0c9f15f79125c3f8
Jacob Earle <jacob.earle@yale.edu>
2020-06-10 16:57:23 -0400

    Finished Javascript tutorial

--- Javascript Basics/ABOUT.md
+++
@@ -1,2 +0,0 @@
-## Javascript Bacics
-In this directory, I will be practicing basic Javascript concepts following along with [this video tutorial](https://www.youtube.com/watch?v=hdI2bqOjy3c).
--- JavascriptBasics/main.js
+++ JavascriptBasics/main.js
@@ -45,4 +45,172 @@ console.log(my_array.pop());
 //Check array index
 console.log(my_array.indexOf('apples'));
 
-//Resume at 30 Minutes
+//Objects in Javascript
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 main street',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+
+//Accessing values
+console.log(person.firstName, person.lastName, person.address);
+
+//pulling out values
+const {firstName} = person;
+console.log(firstName);
+
+//JSON is a format used to communicate through APIs
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    }
+];
+
+const todoJSON = JSON.stringify(todos);
+console.log(todoJSON);
+
+//For loops
+for(let i=0; i < 10; i++){
+    console.log('For loop reached: {$i}');
+}
+
+let i = 0;
+while(i < 10){
+    console.log('While loop reached: {$i}');
+    i++;
+}
+
+//Alternative for loop
+todos.forEach(function(todo){
+    console.log(todo);
+})
+
+//mapping from one array to another
+const todoText = todos.map(function(todo) {
+    return todo.text;
+});
+
+console.log(todoText);
+
+//filtering a list
+const todoComplete = todos.filter(function(todo){
+    return todo.isCompleted == true;
+})
+
+console.log(todoComplete);
+
+//equivalency operators
+const ten = 10;
+
+//doesn't require strict data type agreement
+if(ten == '10'){
+    console.log('This is 10');
+}
+else{
+    console.log('This is not 10');
+}
+
+//does require strict data type agreement
+if(ten === '10'){
+    console.log('This is 10');
+}
+else{
+    console.log('This is not 10');
+}
+
+//Logical Operators
+// ||: or
+// &&: and
+
+//Ternary Operator
+const color = x > 10 ? 'red' : 'blue';
+
+//switch statements
+switch(color){
+    case 'red' :
+        console.log('The color is red.');
+        break;
+    case 'blue' :
+        console.log('The color is blue!');
+        break;
+    default:
+        console.log('The color is not red or blue.');
+}
+
+//functions
+//normal
+function add1(num1, num2){
+    return num1 + num2;
+}
+
+//arrow notation
+const add2 = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log('1 + 1 = ' + add1(1, 1));
+console.log('2 + 2 = ' + add2(2, 2));
+
+//Object oriented programming
+//Older way
+//Constructor Function
+function Person(first, last, dob) {
+    this.first = first;
+    this.last = last;
+    this.dob = new Date(dob);
+    //One way to define a function for the class
+    this.getBirthYear = function(){
+        return this.dob.getFullYear();
+    }
+}
+
+//Writing a function using prototype
+Person.prototype.getLastName = function() {
+    return this.lastName;
+}
+
+//Instantiating an object
+const person1 = new Person('John', 'Doe', '1-2-2020');
+console.log(person1.getBirthYear());
+
+
+//Javascipt classes
+//Works the exact same way as the method used above
+/*
+class Person {
+    constructor(first, last, dob) {
+        this.first = first;
+        this.last = last;
+        this.dob = new Date(dob);
+    }
+
+    getBirthYear() {
+        return this.dob.getFullYear();
+    }
+
+    getLastName() {
+        return this.lastName;
+    }
+}
+
+*/
+
+
